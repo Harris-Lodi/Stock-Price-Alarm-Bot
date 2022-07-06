ce64af4eb9a13e00aede12ca572ab6fdd0d642fd
Harris-Lodi <harrislodi90s@gmail.com>
2022-07-06 00:04:21 -0400

    can scrape front page data now

--- stock_scraper.py
+++ stock_scraper.py
@@ -8,10 +8,22 @@ headers={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 
 tickers = ['SPY', 'AMD', 'AAPL', 'NVDA', 'TSLA', 'INTC', 'NFLX']
 stockData = []
 
-# the following 3 were found by inspecting the code for the price 
-data_type = 'div'
-data_class = 'D(ib) Mend(20px)'
-sub_class_data_type = 'fin-streamer'
+# the following were found by inspecting the code for the price 
+data_type_price = 'div'
+data_class_price = 'D(ib) Mend(20px)'
+sub_class_data_type_price = 'fin-streamer'
+
+data_type_details_a = 'div'
+data_class_details_a = 'D(ib) W(1/2) Bxz(bb) Pend(12px) Va(t) ie-7_D(i) smartphone_D(b) smartphone_W(100%) smartphone_Pend(0px) smartphone_BdY smartphone_Bdc($seperatorColor)'
+sub_class_details_a = 'tr'
+sub_data_type_details_a = 'td'
+sub_data_class_details_a = 'Ta(end) Fw(600) Lh(14px)'
+
+data_type_details_b = 'div'
+data_class_details_b = 'D(ib) W(1/2) Bxz(bb) Pstart(12px) Va(t) ie-7_D(i) ie-7_Pos(a) smartphone_D(b) smartphone_W(100%) smartphone_Pstart(0px) smartphone_BdB smartphone_Bdc($seperatorColor)'
+sub_class_details_b = 'tr'
+sub_data_type_details_b = 'td'
+sub_data_class_details_b = 'Ta(end) Fw(600) Lh(14px)'
 
 def getData(symbol):
 
@@ -46,24 +58,46 @@ def getData(symbol):
 
     '''
     # price_main was found by highlighting the price bar, and finding the root common div
-    price_main = soup.find(data_type, {'class': data_class}).find_all(sub_class_data_type)
+    price_main = soup.find(data_type_price, {'class': data_class_price}).find_all(sub_class_data_type_price)
     price = price_main[0].text
     price_change_value = price_main[1].text 
     price_change_percent = price_main[2].text 
+
+    # getting the front page data
+    details_a = soup.find(data_type_details_a, {'class': data_class_details_a}).find_all(sub_class_details_a)
+    details_b = soup.find(data_type_details_b, {'class': data_class_details_b}).find_all(sub_class_details_b)
+
+    # store specific values we need from front page data
+    previous_close = details_a[0].find(sub_data_type_details_a, {'class': sub_data_class_details_a}).text
+    price_open = details_a[1].find(sub_data_type_details_a, {'class': sub_data_class_details_a}).text
+    days_range = details_a[4].find(sub_data_type_details_a, {'class': sub_data_class_details_a}).text
+    yr_range = details_a[5].find(sub_data_type_details_a, {'class': sub_data_class_details_a}).text
+    volume = details_a[6].find(sub_data_type_details_a, {'class': sub_data_class_details_a}).text
+    avg_volume = details_a[7].find(sub_data_type_details_a, {'class': sub_data_class_details_a}).text
+    market_cap = details_b[0].find(sub_data_type_details_b, {'class': sub_data_class_details_b}).text
+    earnings_date = details_b[4].find(sub_data_type_details_b, {'class': sub_data_class_details_b}).text
+    forward_dividends_and_yield = details_b[5].find(sub_data_type_details_b, {'class': sub_data_class_details_b}).text
+    annual_target_est = details_b[7].find(sub_data_type_details_b, {'class': sub_data_class_details_b}).text
 
     # create a dictionary that will be returned from this function that contains the scraped data
     stock = {
         'symbol': symbol,
         'price': price,
         'value-change': price_change_value,
-        'percent-change': price_change_percent
+        'percent-change': price_change_percent,
+        'previous-close': previous_close,
+        'previous-open': price_open,
+        'todays-range': days_range,
+        'annual-range': yr_range,
+        'volume': volume,
+        'avg-volume': avg_volume,
+        'market-cap': market_cap,
+        'earnings-date': earnings_date,
+        'forward-dividends-&-yield': forward_dividends_and_yield,
+        'annual-target-est': annual_target_est
     }
 
     return stock
-
-    # print(price, price_change_value, price_change_percent)
-    # print("The price and it's changes for " + symbol + " are: " + price + ", " + price_change_value + ", " + price_change_percent)
-
 
 # add all dictionary data to a list
 for ticker in tickers:
@@ -77,4 +111,21 @@ for ticker in tickers:
 with open('stockData.json', 'w') as f:
     json.dump(stockData, f)
 
-print('PRocess Finished')
+print('Process Finished')
+
+
+# testing new code:
+
+'''
+
+test = getData('AMD')
+print(len(test))
+print(test[0].text)
+print(test[1].text)
+print(test[2].text)
+print(test[3].text)
+print(test[4].text)
+print(test[5].text)
+print(test[6].text)
+print(test[7].text)
+'''
