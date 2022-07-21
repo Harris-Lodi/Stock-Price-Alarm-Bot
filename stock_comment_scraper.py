e068b004d8dd193ef4f17665b8f2351ec76da158
Harris-Lodi <harrislodi90s@gmail.com>
2022-07-20 22:31:43 -0400

    An attempt to scrape comments, failed to get comment time

---
+++ stock_comment_scraper.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import json
+import pandas as pd 
+
+# for stockanalysis.com use selenium
+# for yahoo finace, this is not enough since the comments page shows limited comments, but might be enough actually for minute level tickers
+
+comments = []
+times = []
+names = []
+
+header={'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0'}
+
+stock = 'AMD'
+
+url = 'https://finance.yahoo.com/quote/'+ stock +'/community?p=' + stock
+r = requests.get(url, headers = header)
+data = BeautifulSoup(r.text, 'html.parser')
+
+# print(data.text)
+
+comments_list = data.find('div', {'class': 'comments-body'})
+comments_data = comments_list.find('ul').find_all('li')
+
+for comment in comments_data:
+    comments_text = comment.find('div', {'class': 'Wow(bw)'}).text
+    comments_header = comment.find('div', {'class': 'Fz(12px) Mend(20px) Mb(5px)'})
+    comments_time = comments_header.text
+    print(comments_time)
+    comments.append(comments_text)
+    names.append(comments_header.text)
+    times.append(comments_time)
+
+# print(comments_data[1])
+# print(comments[1])
+# print(times[1])
+# print(names[1])
