c13a40ae052f35839b54020d47da6211f596b283
Harris-Lodi <harrislodi90s@gmail.com>
2022-09-07 14:00:24 -0400

    Need to clean it up for the stock script

--- Real-time-chart-test.py
+++ Real-time-chart-test.py
@@ -5,8 +5,11 @@ import time
 import matplotlib.pyplot as plt
 import datetime as dt
 from matplotlib.animation import FuncAnimation
+import matplotlib.animation as animation
+import threading
 
 df = pd.DataFrame(columns=['x', 'y'])
+fig = plt.figure()
 plt.style.use('fivethirtyeight')
 
 def add_data():
@@ -16,24 +19,45 @@ def add_data():
 
     df.loc[len(df.index)] = [x, y]
 
-def animate():
+def animate(i):
 
-    x = df['x']
-    y = df['y']
+    
+
+    x = df.index
+    y = df['x']
+    z = df['y']
 
     plt.cla()
 
-    plt.plot(x, y)
-    plt.show()
+    plt.plot(x, y, z)
+
+    plt.title('Test animated graph over index values')
+    plt.ylabel('Values')
+    plt.xticks(rotation=45, ha='right')
+
+    # plt.show()
    
+def plot_animate():
+    ani = animation.FuncAnimation(fig, animate, interval=1000)
+    plt.show()
 
-while True:
+def calculate_df():
 
-    add_data()
-    display(df)
-    animate()
-    
-    time.sleep(3)
+    while True:
+        add_data()
+        display(df)
+        time.sleep(3)
 
-# to add to data frame and then to plot it out will require multi-threading, or another process to handle the graphing,
-# also can help saving the dataframe data in a csv before plotting it, need to work on this later
+
+x = threading.Thread(target=calculate_df, args=())
+# y = threading.Thread(target=plot_animate(), args=())
+
+
+
+x.start()
+# y.start()
+plot_animate()
+x.join()
+# y.join()
+
+# This technically works now, but is a really ugly setup!
